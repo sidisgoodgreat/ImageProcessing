369afb7f3c75fd13a75d0a051428cd241a690418
sidisgoodgreat <98197031+sidisgoodgreat@users.noreply.github.com>
2024-02-07 23:48:51 -0800

    Update ApImage.java
    I want to watch the superbowl :(((((((((((((((((((((((((

--- ApImage.java
+++ ApImage.java
@@ -1,60 +1,78 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
-public class ApImage {
-  public ArrayList<Integer> [][] APImage() {
-      ArrayList<Integer>[][] list = new ArrayList[200][200];
-      return list;
-  }
-  public int[][] APImage(String filename) {
-    int[][] fileStorer = {{1}, {2}};
-    File fileName = new File(filename);
-    Scanner in = new Scanner(fileName);
-    int j = 0;
-    String[] len = in.nextLine().trim().split("\\s+");
-    for (int i = 0; i < len.length; i++) {
-      j++;
-    }  
-    in.close(); 
-    fileStorer = new int[intLength][intLength];
-    in = new Scanner(inFile);
+//It took me some youtube vids and "research" but I managed to do this so far. I re-did everything cause we did it wrong.
+public class APImage {
+    private Pixel[][] image;
+	
+    public APImage() {
+        image = new Pixel[200][200];
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                image[i][j] = new Pixel(); 
+            }
+        }
+    }
+	
+    public APImage(String filename) {
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            ArrayList<String> lines = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+            scanner.close();
 
-    int lineCount = 0;
-    while (in.hasNextLine()) {
-      String[] currentLine = in.nextLine().trim().split("\\s+"); 
-      for (int i = 0; i < currentLine.length; i++) {
-        fileStorer[lineCount][i] = Integer.parseInt(currentLine[i]);    
-      }
-      lineCount++;
-   }                                 
-   return fileStorer;
-  }
-  public APImage(int width, int height) {
-		image=new Pixel[height][width];
-		for(int i=0;i<image.length;i++) {
-			for(Pixel p: image[i]) {
-				p=new Pixel(0,0,0);
-			}
-		}
-	}
-	
-	public int getWidth() {
-		return image.length;
-	}
-	public int getHeight() {
-		return image[0].length;
-	}
-	public Pixel getPixel(int x, int y) {
-		return image[x][y];
-	}
-	public void setPixel(int x, int y, Pixel p) {
-		image[x][y]=p;
-	}
-	
-	
-	public APImage clone() {
-		return
-	}
+            image = new Pixel[lines.size()][lines.get(0).split("\\s+").length];
 
- 
+        } catch (FileNotFoundException error) {
+            System.err.println("FileNotFoundException: " + error.getMessage());
+
+            image = new Pixel[200][200]; 
+            for (int i = 0; i < image.length; i++) {
+                for (int j = 0; j < image[i].length; j++) {
+                    image[i][j] = new Pixel(); 
+                }
+            }
+        }
+    }
+
+
+    public int getWidth() {
+        return image[0].length;
+    }
+
+    public int getHeight() {
+        return image.length;
+    }
+    public Pixel getPixel(int x, int y) {
+        return image[y][x];
+    }
+    public void setPixel(int x, int y, Pixel z) {
+        image[y][x] = z;
+    }
+    public void draw() {
+        // Idk how to do this tbh
+    }
+
+
+    public APImage clone() {
+        APImage clonedImage = new APImage(this.getWidth(), this.getHeight());
+        for (int i = 0; i < this.getHeight(); i++) {
+            for (int j = 0; j < this.getWidth(); j++) {
+                clonedImage.setPixel(j, i, this.getPixel(j, i)); 
+            }
+        }
+        return clonedImage;
+    }
+
+
+    public String toString() {
+        return "APImage{" + "width=" + getWidth() + ", height=" + getHeight() + '}'; // depends on how we plan to do this
+    }
+
+}
 
         
