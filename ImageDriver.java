89d0c8b072cce79bf9b58a48776462ff57ae5358
ethannguyen1446 <149990000+ethannguyen1446@users.noreply.github.com>
2024-02-11 23:35:51 -0800

    Update ImageDriver.java

--- ImageDriver.java
+++ ImageDriver.java
@@ -1,6 +1,19 @@
+package ImageProcessing;
+
+/**
+ * The ImageDriver class contains the main method to run the image processing application
+ * It initializes a menu object invokes its menu method to display the menu
+ * @author Manh Hang
+ */
 public class ImageDriver {
-  public static void main (String [] args) {
-    Menu m = new Menu();
-    m.menu();
-  }
-   
+	/**
+	 * The main method of the ImageDriver class  
+	 * It creates a menu object and calls on it to display the menu
+	 * 
+	 * @param args The command-line arguments
+	 */
+	public static void main (String [] args) {
+	    Menu m = new Menu();
+	    m.menu();
+	  }
+}
